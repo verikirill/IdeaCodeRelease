406e6cec87845e6fef504ebed16b60c55bf5a961
Andy <obrezkov.andy@gmail.com>
2025-04-20 16:21:50 +0300

    fdjkfndsf

--- Backend/server.py
+++ Backend/server.py
@@ -38,4 +38,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="89.169.1.14", port=8000, reload=True)
