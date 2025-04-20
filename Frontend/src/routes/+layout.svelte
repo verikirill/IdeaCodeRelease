ec57fcf344c83c7c1d68d64186bbb41ca1d8b153
Wasby <vpolkin31@gmail.com>
2025-04-20 10:38:10 +0300

    design_fix

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -105,11 +105,12 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: url('/rasp_background.png');
+    background-image: url('/background.png');
     background-size: cover;
     background-position: center center;
     background-repeat: no-repeat;
     z-index: -1;
+    opacity: 0.7;
   }
 
   .top-header {
